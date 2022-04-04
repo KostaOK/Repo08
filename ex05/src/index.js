0f479381b02cdbe0b88f09ff68abb0909eb16a2e
Niko Kostič <kostic.niko@icloud.com>
2022-04-04 22:02:29 +0200

    Understanding Undefined Value

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,18 +1,19 @@
 var sum = 0;
 function addThree() {
     sum = sum + 3;
-    console.log(sum);
+    console.log("sum from addThree: " + sum);
 }
 
 var sum =5;
 function addFive() {
     sum = sum + 5;
-    console.log(sum);
+    console.log("sum from addFive: " + (sum));
 
 }
 
 addThree();
 addFive();
+
 module.exports = {
     addThree,
     addFive
